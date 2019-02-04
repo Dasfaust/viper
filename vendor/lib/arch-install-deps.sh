b0fbf928a2a77c8e4ee0455631d998d015a02b3c
dasfaust <ccaudill23@gmail.com>
2019-02-04 23:39:33 +0000

    Remove .gitignore files

--- vendor/lib/arch-install-deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Use if you want system-level libraries
-#pacman -S intel-tbb vulkan-icd-loader vulkan-devel glfw-x11 boost
-
-# Vulkan is not distributed with V3
-pacman -S vulkan-icd-loader vulkan-devel
