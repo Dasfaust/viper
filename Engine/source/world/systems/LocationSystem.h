0162963ed3593f1dea968cfa99b1dc9e0593f35f
Cody Caudill <ccaudill23@gmail.com>
2019-04-19 07:37:23 -0400

    Rudimentary collision

--- Engine/source/world/systems/LocationSystem.h
+++ Engine/source/world/systems/LocationSystem.h
@@ -8,17 +8,17 @@ class LocationSystem : public ECS::System
 public:
 	inline void init(ECS::Container* container, World* world) override
 	{
+		
+
 		//debug("Location init");
 		setTickFunction([](double dt, ECS::Component* component, ECS::System* system, ECS::Container* container, World* world)
 		{
 			LocationComponent* comp = reinterpret_cast<LocationComponent*>(component);
 
 			glm::vec3 loc = comp->location;
-			//loc.x += dt * 0.004;
-			loc.z += dt * 0.003;
 
-			ECS::Changeset change = { comp->index, 0, loc };
-			world->changesets[comp->type_id].enqueue(change);
+			/*ECS::Changeset change = { comp->index, 0, loc };
+			world->changesets[comp->type_id].enqueue(change);*/
 
 			MapCell cell = { container->getEntity(comp->entity), glm::vec2(comp->location.x, comp->location.y) };
 			world->mapGridUpdates.enqueue(cell);
--- Server/source/Server.cpp
+++ Server/source/Server.cpp
@@ -6,7 +6,56 @@
 #include "networking/Networking.h"
 #include "console/ConsoleInput.h"
 
+struct VehicleMotionComponent : public ECS::Component { };
 
+class VehicleMotionSystem : public ECS::System
+{
+	std::shared_ptr<ECS::TypeInfo> loc_t;
+
+	inline void init(ECS::Container* container, World* world) override
+	{
+		loc_t = container->resolveType<LocationComponent>();
+
+		setTickFunction([](double dt, ECS::Component* component, ECS::System* system, ECS::Container* container, World* world)
+		{
+			auto sys = reinterpret_cast<VehicleMotionSystem*>(system);
+			ECS::Entity* entity = container->getEntity(component->entity);
+			auto comp = reinterpret_cast<VehicleMotionComponent*>(component);
+
+			LocationComponent* loc = nullptr;
+			if (entity->components.count(sys->loc_t->id))
+			{
+				loc = container->getComponent<LocationComponent>(entity);
+				int x = floor(loc->location.x);
+				int y = floor(loc->location.y);
+
+				bool canMove = true;
+				for (int i = x; i > x - 1; i--)
+				{
+					int cell = floor(loc->location.y) * world->mapWidth + x;
+					for (auto c : world->map[cell])
+					{
+						if (c.entity != 0 && c.entity->index != entity->index)
+						{
+							canMove = false;
+						}
+					}
+				}
+
+				if (canMove)
+				{
+					glm::vec3 velocity = loc->location;
+					velocity.z -= 0.002;
+
+					ECS::Changeset change = { loc->index, 0, velocity };
+					world->changesets[loc->type_id].enqueue(change);
+				}
+			}
+		});
+
+		setWaitFunction([](ECS::System* system, World* world) { });
+	};
+};
 
 class Game : public V3Application
 {
@@ -21,24 +70,36 @@ public:
 		world = v3->getModule<World>();
 
 		world->createSystem<LocationSystem, LocationComponent>(0);
-		world->createSystem<RenderSystem, RenderComponent>(1);
+		world->createSystem<VehicleMotionSystem, VehicleMotionComponent>(1);
+		world->createSystem<RenderSystem, RenderComponent>(2);
 	};
 
 	inline void onTick() override
 	{
 		if (entities.size() == 0)
 		{
-			for (int i = 0; i < 2; i++)
+			/*for (int i = 0; i < 2; i++)
 			{
-				entities.push_back(world->createEntity<LocationComponent, RenderComponent>([](uint32 index, ECS::Component *comp)
+				
+			}*/
+
+			entities.push_back(world->createEntity<LocationComponent, VehicleMotionComponent, RenderComponent>([](uint32 index, ECS::Component *comp)
+			{
+				if (index == 0)
 				{
-					if (index == 0)
-					{
-						auto c = reinterpret_cast<LocationComponent*>(comp);
-						c->location.x = rand() % (30 - 1 + 1) + 1;
-					}
-				})->get());
-			}
+					/*auto c = reinterpret_cast<LocationComponent*>(comp);
+					c->location.x = rand() % (30 - 1 + 1) + 1;*/
+				}
+			})->get());
+
+			entities.push_back(world->createEntity<LocationComponent, RenderComponent>([](uint32 index, ECS::Component *comp)
+			{
+				if (index == 0)
+				{
+					auto c = reinterpret_cast<LocationComponent*>(comp);
+					c->location.z = -200.0f;
+				}
+			})->get());
 
 			debug("Entities created");
 		}
